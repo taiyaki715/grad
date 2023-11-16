53c4822b8b20afc8a5568d72229f9c199b30ca4e
Taiki Ishikawa <taiyaki715@icloud.com>
2023-11-16 14:58:59 +0900

    M1のGPUで実行するように変更

--- src/train.py
+++ src/train.py
@@ -11,6 +11,10 @@ data_loader = torch.utils.data.DataLoader(dataset, batch_size=8, shuffle=True)
 # モデルの定義
 model = Model()
 
+# モデルをGPUに転送
+device = torch.device('mps');
+model.to(device);
+
 # 損失関数
 criterion = torch.nn.MSELoss()
 
@@ -20,11 +24,14 @@ optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 # 学習するエポック数
 num_epochs = 10
 
-for epoch in range(num_epochs):
+losses = []
+
+for current_epoch, epoch in enumerate(range(num_epochs)):
   running_loss = 0.0
 
-  for batch in data_loader:
+  for i, batch in enumerate(data_loader):
     inputs, targets = batch
+    inputs, targets = inputs.to(device), targets.to(device)
     optimizer.zero_grad()
     outputs = model(inputs)
     loss = criterion(outputs, targets)
@@ -32,20 +39,9 @@ for epoch in range(num_epochs):
     optimizer.step()
     running_loss += loss.item()
 
-    with torch.no_grad():
-      outputs = model(inputs)
-      plt.figure(figsize=(16, 16))
-      plt.subplot(1, 3, 1)
-      plt.title('Input')
-      plt.imshow(inputs[0].permute(1, 2, 0).numpy() * 255.0)
-      plt.subplot(1, 3, 2)
-      plt.title('Output')
-      plt.imshow(outputs[0].numpy().squeeze() * 255.0)
-      plt.subplot(1, 3, 3)
-      plt.title('Target')
-      plt.imshow(targets[0].numpy().squeeze() * 255.0)
+    print(f"Epoch:{current_epoch + 1}/{num_epochs} Batch:{i}/{len(data_loader)} Loss:{running_loss / (i + 1)}")
 
-      plt.savefig(f'result.png')
-      plt.close()
+  losses.append(running_loss / len(data_loader))
+  print(f'Epoch: [{epoch + 1}/{num_epochs}] Loss: {running_loss / len(data_loader)}')
 
-  print(f'Epoch [{epoch + 1}/{num_epochs}] Loss: {running_loss / len(data_loader)}')
+print(losses)
