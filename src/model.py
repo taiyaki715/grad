0cbcdd847615717633b430ff34279ccdd069140a
Taiki Ishikawa <taiyaki715@icloud.com>
2023-11-17 10:11:59 +0900

    reluを追加

--- src/model.py
+++ src/model.py
@@ -31,35 +31,45 @@ class Head(torch.nn.Module):
 
     # ダウンサンプリング側
     self.downsample_conv = torch.nn.Conv2d(3, 1, kernel_size=3, stride=1, padding=1)
+    self.downsample_relu = torch.nn.ReLU()
     self.downsample1 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
     self.downsample2 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
     self.downsample3 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
 
     # アップサンプリング側
     self.conv1 = torch.nn.Conv2d(1, 128, kernel_size=3, stride=1, padding=1)
+    self.relu1 = torch.nn.ReLU()
     self.upsample1 = torch.nn.Upsample(scale_factor=2, mode='nearest')
     self.dropout1 = torch.nn.Dropout2d(p=0.5)
     self.conv2 = torch.nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
+    self.relu2 = torch.nn.ReLU()
     self.upsample2 = torch.nn.Upsample(scale_factor=2, mode='nearest')
     self.dropout2 = torch.nn.Dropout2d(p=0.5)
     self.conv3 = torch.nn.Conv2d(64, 1, kernel_size=3, stride=1, padding=1)
+    self.relu3 = torch.nn.ReLU()
     self.upsample3 = torch.nn.Upsample(scale_factor=2, mode='nearest')
+    self.sigmoid = torch.nn.Sigmoid()
 
   def forward(self, encoder_outputs, original_inputs):
     down_x_1 = self.downsample_conv(original_inputs)
+    down_x_1 = self.downsample_relu(down_x_1)
     down_x_2 = self.downsample1(down_x_1)
     down_x_3 = self.downsample2(down_x_2)
     down_x_4 = self.downsample3(down_x_3)
 
     x = torch.reshape(encoder_outputs, (-1, 1, 64, 64)) + down_x_4
     x = self.conv1(x)
+    x = self.relu1(x)
     x = self.upsample1(x) + down_x_3
     x = self.dropout1(x)
     x = self.conv2(x)
+    x = self.relu2(x)
     x = self.upsample2(x) + down_x_2
     x = self.dropout2(x)
     x = self.conv3(x)
+    x = self.relu3(x)
     x = self.upsample3(x) + down_x_1
+    # x = self.sigmoid(x)
 
     return x
 
